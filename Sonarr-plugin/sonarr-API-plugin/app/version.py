1e2f0dbbe7e91003e1bd9dafe84921595432d0c4
wasswa joseph <wasswajose9@gmail.com>
2025-11-23 14:22:30 +0300

    Release Sonarr Plugin v1.1.18

--- Sonarr-plugin/sonarr-API-plugin/app/version.py
+++ Sonarr-plugin/sonarr-API-plugin/app/version.py
@@ -1,4 +1,4 @@
 """Version information for the Sonarr-Seedr Integration."""
-__version__ = "1.1.17"
-__build_date__ = "2025-11-21"
+__version__ = "1.1.19"
+__build_date__ = "2025-11-23"
 __description__ = "Sonarr-Seedr Integration - Automated torrent downloading via Seedr cloud service"
